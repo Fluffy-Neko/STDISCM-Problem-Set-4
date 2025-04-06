68fc1e7a7adc6dffe769496727bbda9fb9cbfe24
BruceBM <61485567+BruceBM@users.noreply.github.com>
2025-04-06 20:06:19 +0800

    UI Improvements

--- OnlineEnrollmentSystem/OnlineEnrollmentSystem.Data/obj/Debug/net9.0/OnlineEnrollmentSystem.Data.AssemblyInfo.cs
+++ OnlineEnrollmentSystem/OnlineEnrollmentSystem.Data/obj/Debug/net9.0/OnlineEnrollmentSystem.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineEnrollmentSystem.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f205d71cbda1f6e91e96d127c3081052a115f7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a100a479e9ea1e13999e1bf115a033ff416780a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineEnrollmentSystem.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineEnrollmentSystem.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OnlineEnrollmentSystem/OnlineEnrollmentSystem.Views/obj/Debug/net9.0/OnlineEnrollmentSystem.Views.AssemblyInfo.cs
+++ OnlineEnrollmentSystem/OnlineEnrollmentSystem.Views/obj/Debug/net9.0/OnlineEnrollmentSystem.Views.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineEnrollmentSystem.Views")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f205d71cbda1f6e91e96d127c3081052a115f7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a100a479e9ea1e13999e1bf115a033ff416780a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineEnrollmentSystem.Views")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineEnrollmentSystem.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
