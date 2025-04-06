ee67bd6b75c36892a694505e006e5918cc7bb996
Dane Chan <dane_chan@dlsu.edu.ph>
2025-04-07 01:17:08 +0800

    Changed models to match database schema

--- OnlineEnrollmentSystem/OnlineEnrollmentSystem.Data/Models/CourseModel.cs
+++ OnlineEnrollmentSystem/OnlineEnrollmentSystem.Data/Models/CourseModel.cs
@@ -2,8 +2,10 @@ namespace OnlineEnrollmentSystem.Models
 {  
     public class CourseModel  
     {  
-        public int Id { get; set; }  
-        public string Name { get; set; }  
-        public bool IsEnrolled { get; set; }  
+        public int Id { get; set; }
+        public int InstructorId { get; set; }
+        public string CourseCode { get; set; }  
+        public int Units { get; set; }
+        public int Capacity { get; set; }
     }  
 }
